79c81e01d8a5cb5070ba8fe5e43776390efd7f66
Dương Anh Kiệt <duonganhkiet1312@gmail.com>
2022-12-29 07:02:42 +0100

    Update database

--- enseignant/src/main/java/com/example/RessourcesHumaines.java
+++ enseignant/src/main/java/com/example/RessourcesHumaines.java
@@ -25,34 +25,8 @@ public class RessourcesHumaines {
     }
 
     @Transactional
-    public void updateHeures(Long id, Long heures)
-    {
-        entityManager.createNativeQuery("update Enseignant e set e.heures = :heures where e.id = :id")
-                .setParameter("id", id)
-                .setParameter("heures", heures)
-                .executeUpdate();
-    }
-
-    @Transactional
-    public void updateEquivalence(Long id, float equivalence)
-    {
-        entityManager.createNativeQuery("update Enseignant e set e.equivalence = :equivalence where e.id = :id")
-                .setParameter("id", id)
-                .setParameter("equivalence", equivalence)
-                .executeUpdate();
-    }
-
-    @Transactional
     public void addEnseignant(Enseignant enseignant)
     {
-//        entityManager.createNativeQuery("insert into Enseignant (nom, prenom, heures, equivalence, departement, service) values (:nom, :prenom, :heures, :equivalence, :departement, :service)")
-//                .setParameter("nom", enseignant.nom)
-//                .setParameter("prenom", enseignant.prenom)
-//                .setParameter("heures", enseignant.heures)
-//                .setParameter("equivalence", enseignant.equivalence)
-//                .setParameter("departement", enseignant.departement)
-//                .setParameter("service", enseignant.service)
-//                .executeUpdate();
         entityManager.persist(enseignant);
     }
 }
